77e6a08f513af182dc3770027095d3bb89b70476
Jorismar Barbosa <jorismar.barbosa@gmail.com>
2015-10-05 17:31:15 -0300

    Servidor Datagram implementado

--- include/buffer.h
+++ include/buffer.h
@@ -1,68 +1,30 @@
-#include <cstdio>
-#include <cstdlib>
+#ifndef BUFFER_H
+#define BUFFER_H
 
-typedef char * tElement;
+#include <cstdlib>
+#include "datapacket.h"
 
 class Buffer {
     private:
-        int r_pos;          // Posição de Leitura para next();
-        int w_pos;          // Posição de Escrita para add();
-        int buff_size;      // Tamanho do Buffer
-        int elem_size;      // Tamanho do elemento
-        tElement * buffer;  // Tamanho do buffer
+        unsigned int r_pos;          // Posição de Leitura para next();
+        unsigned int w_pos;          // Posição de Escrita para add();
+        unsigned int buff_size;      // Tamanho do Buffer
+        unsigned int data_size;      // Tamanho do elemento
+        DataPacket **buffer;         // Buffer
         
     public:
-        /**
-         * Construtor
-         * 
-         * @param size      tamanho do buffer
-         * @param elem_size tamanho do elemento
-         */
-        Buffer(int size, int elem_size);
-
-        /**
-         * Destrutor
-         */
+        Buffer(unsigned int, unsigned int);
         virtual ~Buffer();
         
-        /**
-        * Lê o próximo elemento do buffer
-        *
-        * @return retorna o próximo elemento do buffer
-        */
-        tElement next();
+        int set(unsigned int, DataPacket*);
+        DataPacket* get(unsigned int);
 
-        /**
-         * Recupera um elemento específico do buffer
-         *
-         * @param index Indice da posição do buffer
-         * @return      Elemento da posição solicitada ou NULL se
-         *              o index for inválido
-         */
-        tElement get(int index);
-
-        /**
-         * Adiciona um novo elemento no Buffer
-         *
-         * @param elem elemento a ser adicionado
-         */
-        void add(tElement elem);        
-
-        /**
-         * Atribui um novo elemento numa posição especifica do buffer
-         *
-         * @param index Indice da posição do buffer
-         * @param elem  Elemento a ser adicionado
-         * @return      0 se bem sucedido ou 1 se o index for inválido
-         */
-        int set(int index, tElement elem);
+        DataPacket* next();
         
+        void add(DataPacket*);      
+        void remove(unsigned int);
         
-        /**
-         * Remove um elemento do buffer
-         *
-         * @param index Indice da posição do buffer
-         * @return      Elemento removido ou NULL se o index for inválido
-         */
-        tElement remove(int index);
+        int size();
 };
+
+#endif
