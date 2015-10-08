7938c25b3b496b3ed1a1b167d1aebd53cd94ad00
Josue <josuepaiva@lavid.ufpb.br>
2015-10-08 12:39:16 -0300

    sdix

--- SegmentosDash/Conversoes.cpp
+++ SegmentosDash/Conversoes.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <cstdlib>
-#include <limits>
+#include <limits.h>
 #include <cctype>
 #include <stdio.h>
+#include <string.h>
 #include "conversoes.h"
+#include <bitset>
 using namespace std;
+
 
 Conversoes::Conversoes(char *path){
 	FILE *file = fopen(path,"rb");
@@ -36,52 +39,110 @@ int Conversoes::bits(char *ar,char j){
     strcat(ar,bits);
     return 0;
 }
-int Conversoes::capturatamanhotipo(){
-	int i = 0;
-	char *end;
-	char str[5];
-	while(i<4){
-		this->lista[i] = str[i];
-		i++;	
-	}
-	str[4] = '\0';
-	return (int)strtol(str,&end,10);
-}
+
 void Conversoes::iniciaAnalize(){
-	FILE * arquivo;
-	arquivo = fopen("test.mp4","wb");
-	if(arquivo==NULL)cout<<"Erro arquivo de escrita\n";
+	//FILE * arquivo;
+	//arquivo = fopen("test.mp4","wb");
+	//if(arquivo==NULL)cout<<"Erro arquivo de escrita\n";
 	int i = 0;
 	int cont = 0;
 	char str[5];
-	std::string  identificador;
-		if(capturatamanhotipo() == 0){
-			int version = 0;
-			fwrite(version,sizeof(int),sizeof(version),arquivo);
-			cont = 4;
-			while(i<4){
-				str[i] = this->lista[cont];
+	//Verifica tamanho e versao da caixa os primeiros quatro bytes
+	this->isobox.version = this->lista[i];
+	if(isobox.version == 0){
+		cout<<"Campos serao de 32 bits\n";
+		// Depois do identificador ler 4 bytes
+		i++;
+		while(cont<3){
+			cont++;
+			if((int)this->lista[i] == 0){
+				i++;
+			}else{
+				this->isobox.size = (int)this->lista[i];
+			}
+		}
+		i++;
+		cont = 0;
+		while(cont <4){
+			str[cont] = this->lista[i];
+			cont++;
+			i++;
+		}
+		str[4] = '\0';
+		cont = 0;
+		if(strcmp(str,"ftyp") ==  0){
+			while(cont<4){
+				str[cont] = this->lista[i];
 				i++;
 				cont++;
 			}
 			str[4] = '\0';
-			if(!strcmp(str,"styp")){
-				cout<<"Segmento de Media\n";
-				std::string major_brand;
-				while(i<4){
-
+			cont= 0;
+			this->ftyp.major_brand = str;
+		}else if(strcmp(str,"styp") == 0){
+				cont = 0;
+				while(cont<4){
+					str[cont] = this->lista[i];
+					cont++;
+					i++;
 				}
-			}else if(!strcmp(str,"ftyp")){
-				cout<<"Segmento de Inicializacao\n";
-			}else{
-				cout<<"Desconhecido\n";
-			}
-		}else if(capturatamanhotipo() == 1){
-			int version = 1;
+				str[4] = '\0';
+				this->styp.major_brand = str;
+				this->styp.menor_version = (int)this->lista[i];
+				//Captura marcas compativeis
+				cont =0;
+				//i++;
+				cout<<"GGG"<<this->lista[i];
+				while(cont<4){             
+					this->styp.compatible_brand.push_back(this->lista[i]);
+					cont++;
+					i++;
+				}
+				cont = 0;
+				while(cont<4){
+					  this->styp.compatible_brand.push_back(this->lista[i]);
+					cont++;
+					i++;
+				}
+				/*cout<<"Primeiro caractere da caixa sdix "<<(int)this->lista[24];
+				cout<<"\nSegundo caractere da caixa sdix "<<(int)this->lista[25];
+				cout<<"\nTerceiro caractere da caixa sdix "<<(int)this->lista[26];
+				cout<<"\nQuarto caractere da caixa sdix "<<(int)this->lista[27];*/
+				
+				cout<<"\n\n";
+				cont = 0;
+				this->sdix.version = (int)this->lista[i];
+				if(sdix.version == 0){
+					cout<<"A caixa sdix possui campos de 32bits\n";
+					cout<<i;
+					
+				}
+				if(sdix.version == 1){
+					cout<<"A caixa sdix possui campos de 64bits\n";
+					cont = 0;
+					while(cont<3){
+						cont++;
+						if((int)this->lista[i] == 0){
+							i++;
+						}else{
+							this->sdix.size = (int)this->lista[i];
+						}
+					}
+				cont = 0;
+				
+				}
+				cout<<"Marcas adicionadas "<<this->styp.compatible_brand[0]<<" "<<this->styp.compatible_brand[1];
+				
 		}else{
-
-			cout<<"Tem algo errado";
+			cont = 0;
+			i++;
+			cout<<"Nao leva styp\n";
 		}
+	//cout<<"Tamanho da caixa iso "<<this->isobox.size<<" bytes\n";		
+	}else if(isobox.version == 1){
+		cout<<"Campos serão de 64 bits\n";
+		//Depois do identificador ler 8 bytes
+	}
 }
 
 void Conversoes::segmentInitialization(){
--- SegmentosDash/Conversoes.h
+++ SegmentosDash/Conversoes.h
@@ -1,7 +1,39 @@
-#include "lista.h"
 #include "boxftyp.h"
 #include <vector>  //for std::vector
 #include <array> //for std::array
+
+typedef struct{
+	int version;
+	int size;
+}Isobox;
+
+typedef struct{
+	std::string major_brand;
+	int menor_version;
+	std::vector <std::string> compatible_brand;
+}Ftyp;
+
+typedef struct{
+	std::string major_brand;
+	int menor_version;
+	std::vector <char> compatible_brand;
+}Styp;
+
+typedef struct{
+	std::string major_brand;
+	int menor_version;
+	std::vector <std::string> compatible_brand;
+}Unknown;
+
+typedef struct{
+	int version;
+	int size;
+	int flags;
+	int reference_id;
+	int timescale;
+	int earliest_presentation_time;
+	int first_offset;
+}Sdix;
 
 class Conversoes{
 	private:
@@ -10,11 +42,14 @@ class Conversoes{
 		FILE *arquivo;
 		Ftype fftype;
 		std::vector <char> lista;
+		Isobox isobox;
+		Ftyp ftyp;
+		Styp styp;
+		Sdix sdix;
 		
 	public:
 		Conversoes(char *);
 		~Conversoes();
-		int capturatamanhotipo();
 		int capturacaixa();
 		void iniciaAnalize();
 		void segmentInitialization();
